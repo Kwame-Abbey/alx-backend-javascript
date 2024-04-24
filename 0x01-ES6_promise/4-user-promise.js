1ad466307ba2d4026fd325673cfe83ade01d2d54
Kwame-Abbey <constancekwameabbey@gmail.com>
2024-04-24 11:21:30 +0100

    Simplified code

--- 0x01-ES6_promise/4-user-promise.js
+++ 0x01-ES6_promise/4-user-promise.js
@@ -1,8 +1,6 @@
 export default function signUpUser(firstName, lastName) {
-  return new Promise((resolve, reject) => { // eslint-disable-line no-unused-vars
-    resolve({
-      firstName,
-      lastName,
-    });
+  return Promise.resolve({
+    firstName,
+    lastName,
   });
 }
