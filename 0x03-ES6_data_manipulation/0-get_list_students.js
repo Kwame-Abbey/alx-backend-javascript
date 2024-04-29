5cfb1e5619803fbc5a53a8462c3cf603c12bee79
Kwame-Abbey <constancekwameabbey@gmail.com>
2024-04-29 11:38:55 +0100

    An array of objects

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,9 @@
+export default function (getListStudents) { // eslint-disable-line no-unused-vars
+  const obj1 = { id: 1, firstName: 'Guillaume', location: 'San Francisco' };
+  const obj2 = { id: 2, firstName: 'James', location: 'Columbia' };
+  const obj3 = { id: 5, firstName: 'Serena', location: 'San Francisco' };
+
+  const arrayOfObjects = [obj1, obj2, obj3];
+
+  return arrayOfObjects;
+}
